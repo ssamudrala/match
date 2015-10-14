8dfb3948709b42a94185bd993109508d9214aea7
Jeff Shaw <jeffrey.b.shaw@intel.com>
2015-10-14 08:24:43 -0700

    bump to development version 0.0.3dev
    Signed-off-by: Jeff Shaw <jeffrey.b.shaw@intel.com>

--- include/match_version.h
+++ include/match_version.h
@@ -59,7 +59,7 @@
  * formulate a complete version number. The version number will be higher
  * for more recent versions.
  */
-#define MATCH_VER_EXTRA  15
+#define MATCH_VER_EXTRA  16
 
 /*
  * SUFFIX can be
@@ -67,7 +67,7 @@
  *   ""     - for released versions
  *   "rc"  - for release candidates
  */
-#define MATCH_VER_SUFFIX ""
+#define MATCH_VER_SUFFIX "dev"
 
 #define MATCH_VER_NUM(a,b,c,d) ((a) << 24 | (b) << 16 | (c) << 8 | (d))
 
